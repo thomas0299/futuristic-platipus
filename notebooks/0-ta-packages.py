8c325341f6ef74c8e5ee957a62fcd3f385c2e8fd
thomas0299 <thomas0299@users.noreply.github.com>
2022-07-02 21:02:01 +0000

    deploy: e01bf3b4167927cc028604c3176e93dcf1577d03

---
+++ notebooks/0-ta-packages.py
@@ -0,0 +1,15 @@
+#importing all relevant packages for whole project
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import requests
+import os
+from sodapy import Socrata
+from IPython.core.display import HTML
+from IPython.display import YouTubeVideo
+import plotly.express as px
+import plotly.graph_objs as go
+from datetime import datetime, date
+from plotly.subplots import make_subplots
