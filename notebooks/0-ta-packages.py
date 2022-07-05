74d5a1c6d294368e29534a255a9542f7dfa314ce
thomas0299 <thomas0299@users.noreply.github.com>
2022-07-05 10:32:57 +0000

    deploy: 73e1d9d10a6c02123300bf92c61288eff2cfbb09

--- notebooks/0-ta-packages.py
+++ notebooks/0-ta-packages.py
@@ -1,15 +1,25 @@
-#importing all relevant packages for whole project
+#importing all relevant packages for whole capstone project
 
+#for data wrangling
 import numpy as np
 import pandas as pd
+from datetime import datetime, date
+
+#for visualisations
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import plotly.express as px
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+
+#for api requests
 import requests
 import os
 from sodapy import Socrata
-from IPython.core.display import HTML
-from IPython.display import YouTubeVideo
-import plotly.express as px
-import plotly.graph_objs as go
-from datetime import datetime, date
-from plotly.subplots import make_subplots
+
+#for modelling
+
+
+
+
